edcd78f835de33db20589eef9b07a50982d76492
kevinotsuka <otsukasan@gmail.com>
2016-04-29 11:56:59 -0400

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'stylish_portfolios/index'
-  root 'welcome#index'
+  root 'stylish_portfolios#index'
+ # root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
