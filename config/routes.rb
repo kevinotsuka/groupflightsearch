106fc1cf9712eba338616c6e581b64fd3f9028f2
kevinotsuka <otsukasan@gmail.com>
2016-04-29 11:52:36 -0400

    fixing typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'stylish_portfolios/index'
+  root 'stylish_portfolios#index'
   get 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
