dec88874b6aef6ce89cd1ccfbf79b90727755083
kevinotsuka <otsukasan@gmail.com>
2016-04-29 11:49:57 -0400

    commit to heroku

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,6 +2,7 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.precompile += %w( stylish_portfolio/manifest.css stylish_portfolio/manifest.js )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
